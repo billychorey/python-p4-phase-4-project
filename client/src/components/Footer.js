7d461250dd228c87727649d4a1e46e067c9881e7
Billy <billychorey@gmail.com>
2024-09-27 01:42:08 -0400

    clean up login

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,15 @@
 // client/src/components/Footer.js
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Footer = ({ user }) => {
-  const [currentDate, setCurrentDate] = useState('');
-
-  useEffect(() => {
-    // Set current date
-    const date = new Date();
-    setCurrentDate(date.toLocaleDateString()); // Format date as 'MM/DD/YYYY'
-  }, []);
-
   return (
     <div className="footer">
-      <p>{`Great to see you again, ${user ? `${user.first_name}` : 'Guest'}!`}</p>
-      <p>{`Today's Date: ${currentDate}`}</p>
+      {user ? (
+        <>
+          <p>Great to see you again, {user.first_name}!</p>
+          <p>Today's Date: {new Date().toLocaleDateString()}</p>
+        </>
+      ) : null} {/* Remove the else content */}
     </div>
   );
 };
